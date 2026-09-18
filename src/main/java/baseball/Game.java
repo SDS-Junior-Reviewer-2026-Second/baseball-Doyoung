f5e84499b7fddac75d027bec166894a31ca9ab37
kimdoyoung0319 <kimdoyoung0319@gmail.com>
2026-09-18 11:23:09 +0900

    Added test cases

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,16 @@
 package baseball;
 
+record QueryResult(boolean solved, int balls, int strikes) {}
+
 public class Game {
 
+    private String answer;
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public QueryResult query(String numbers) {
+        return new QueryResult(false, 0, 0);
+    }
 }
--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,51 +1,71 @@
 package baseball;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class GameTest {
 
-	@Test
-	public void 입력값이_없을_경우() {
-	}
-	
-	@Test
-	public void 입력값_자리수가_세자리가_아닐�경우() {
+    private final String ANSWER = "123";
 
-	}
-	
-	@Test
-	public void 입력값에_숫자_외의_뮸자가_입력될_경우() {
+    private Game game = new Game();
 
-	}
-	
-	@Test
-	public void 입력값에_중복된_숫자가_입력될_경우() {
+    @BeforeEach
+    public void setupAnswer() {
+        game.setAnswer(ANSWER);
+    }
 
-	}
-	
-	@Test
-	public void 숫자_세개가_전부_일치_할_경우_3_strike() {
+    @Test
+    public void 입력값이_없을_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.query("");
+        });
+    }
 
-	}
-	
-	@Test
-	public void 숫자_세개가_전부_일치_하지_않을_경우_0_strike_0_ball() {
+    @Test
+    public void 입력값_자리수가_세자리가_아닐_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.query("12");
+        });
+    }
 
-	}
-	
-	@Test
-	public void 스트라이크만_있을_경우_1_strike_0_ball() {
+    @Test
+    public void 입력값에_숫자_외의_문자가_입력될_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.query("Hello, world!");
+        });
+    }
 
-	}
-	
-	@Test
-	public void 볼만_있을_경우_0_strike_1_ball() {
+    @Test
+    public void 입력값에_중복된_숫자가_입력될_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.query("112");
+        });
+    }
 
-	}
-	
-	@Test
-	public void 볼과_스트라이크가_함께_있을_경우_1_strike_1_ball() {
+    @Test
+    public void 숫자_세개가_전부_일치_할_경우_3_strike() {
+        QueryResult expected = new QueryResult(true, 0, 3);
+        QueryResult actual = game.query(ANSWER);
 
-	}
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void 숫자_세개가_전부_일치_하지_않을_경우_0_strike_0_ball() {
+        QueryResult expected = new QueryResult(false, 0, 0);
+        QueryResult actual = game.query("789");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void 스트라이크만_있을_경우_1_strike_0_ball() {}
+
+    @Test
+    public void 볼만_있을_경우_0_strike_1_ball() {}
+
+    @Test
+    public void 볼과_스트라이크가_함께_있을_경우_1_strike_1_ball() {}
 }
